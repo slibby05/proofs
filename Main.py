4e7dd2e98e7561eccf0561384cc8dcca11f9ecb9
Steven Libby <slibby@pdx.edu>
2019-10-08 16:15:19 -0700

    added initial files

--- Main.py
+++ Main.py
@@ -9,8 +9,7 @@ from Proof import (clear, step, premise, andI, andEL, andER, \
 def main():
     try:
         andComm().print_proof()
-        orComm().print_proof()
-        doubleNeg().print_proof()
+        arrTrans().print_proof()
     except (ProofException) as e:
         e.print()
 
@@ -52,37 +51,26 @@ def andComm():
     # l3 = andI(l2,l1,And(b,a))
     # return l3
     
-def orComm():
-    clear()
-    x = Var("x")
-    y = Var("y")
-    p1 = premise(Or(x,y))
-    l1 = assume(x)
-    l2 = assumed(x)
-    l3 = orIR(l2, Or(y,x))
-    l4 = arrowI(l1,l3, Arrow(x,Or(y,x)))
-    l5 = assume(y)
-    l6 = assumed(y)
-    l7 = orIL(l6, Or(y,x))
-    l8 = arrowI(l5,l7, Arrow(y,Or(y,x)))
-    l9 = orE(p1,l4,l8,Or(y,x))
-    return l9
 
-def doubleNeg():
-    clear()
-    a = Var("a")
-    p1 = premise(Not(Not(a)))
-    l1 = LEM(Or(a,Not(a)))
-    l2 = assume(a)
-    l3 = assumed(a)
-    l4 = arrowI(l2,l3,Arrow(a,a))
-    l5 = assume(Not(a))
-    l6 = assumed(Not(a))
-    l7 = notE(l6,p1,false())
-    l8 = FE(l7,a)
-    l9 = arrowI(l5,l8,Arrow(Not(a),a))
-    l10 = orE(l1,l4,l9,a)
-    return l10
+def arrTrans():
+    a = Var("A")
+    b = Var("B")
+    c = Var("C")
+
+    p1 = premise(Arrow(a,b))
+    p2 = premise(Arrow(b,c))
+
+    a1 = assume(a)
+
+    return arrowI(a1, arrowE(arrowE(assumed(a), p1, b), p2, c), Arrow(a,c))
+
+    # linear (3-column) proof
+    # l1 = assumed(a)
+    # l2 = arrowE(l1, p1, b)
+    # l3 = arrowE(l2, p2, c)
+    # l4 = arrowI(a1, l3, Arrow(a,c))
+    # return l4
+
 
 if __name__ == "__main__":
     main()
